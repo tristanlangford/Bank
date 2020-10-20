6109c1539138428c4a7c152150f1c71b56277717
Tristan Langford <tristanlangford@hotmail.com>
2020-10-20 17:52:16 +0100

    tested Account Api functions

--- Bank/ClientApp/src/components/Account/Account.test.js
+++ Bank/ClientApp/src/components/Account/Account.test.js
@@ -6,6 +6,7 @@ import { Account } from './Account';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
+import { isPropertySignature } from 'typescript';
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -19,10 +20,18 @@ describe("Account", function(){
     }
 
     let wrapper;
+
+    beforeAll(function() {
+        global.fetch = jest.fn();
+    })
         
     beforeEach(() => wrapper = shallow(
-            <Account/>
+            <Account match={{params: {id: 1}}}/>, { disableLifecycleMethods: true }
         ));
+
+    afterEach(() => {
+        wrapper.unmount();
+        });
 
 
     it("has correct starting state", async function() {
@@ -35,5 +44,73 @@ describe("Account", function(){
         wrapper.setState({ Account: accountMock, Loading: false })
         expect(wrapper.find('.balance').text()).toEqual("Balance: £10")
     })
+
+    it("update accounts on withdrawal", (done) => {
+
+        const spyDidMount = jest.spyOn(Account.prototype,"withdrawal");
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                status: 200,
+                json: () => {
+                return Promise.resolve(accountMock);
+                }
+            });
+        });
+        const didMount = wrapper.instance().withdrawal("event");
+
+        didMount.then(() => {
+            wrapper.update();
+            expect(wrapper.state('Account')).toEqual(accountMock)
+            spyDidMount.mockRestore();
+            fetch.mockClear();
+            done();
+       });
+    })
+
+    it("update accounts on deposit", (done) => {
+
+        const spyDidMount = jest.spyOn(Account.prototype,"deposit");
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                status: 200,
+                json: () => {
+                return Promise.resolve(accountMock);
+                }
+            });
+        });
+        const didMount = wrapper.instance().deposit("event");
+
+        didMount.then(() => {
+            wrapper.update();
+            expect(wrapper.state('Account')).toEqual(accountMock)
+            spyDidMount.mockRestore();
+            fetch.mockClear();
+            done();
+       });
+    })
+
+    it("update accounts on component call", (done) => {
+
+        const spyDidMount = jest.spyOn(Account.prototype,"getAccount");
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                status: 200,
+                json: () => {
+                return Promise.resolve(accountMock);
+                }
+            });
+        });
+        const didMount = wrapper.instance().getAccount();
+
+        didMount.then(() => {
+            // updating the wrapper
+            wrapper.update();
+            expect(wrapper.state('Account')).toEqual(accountMock)
+            spyDidMount.mockRestore();
+            fetch.mockClear();
+            done();
+       });
+    })
+
 
 })
